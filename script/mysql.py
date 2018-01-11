9783d0e8ed4ba6f077685b8017546a1cfc8e0191
Ronnie <harddies827@126.com>
2018-01-11 10:12:26 +0800

    python学习

---
+++ script/mysql.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import MySQLdb
+
+try
+	MySQLdb.connect(host="120.26.63.239",user="glitzhome",passwd="123glitzhome",port=3307,db="gzcloud",charset="utf8")
